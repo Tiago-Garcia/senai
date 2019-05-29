f1dd99be54950e110487c24365fd90b31e874aff
jaqueline.correia <jaqueline.correia@UO692M4075565.acad692.com.br>
2019-05-29 20:50:30 -0300

    metodos de get e set implementados

--- ExercicioAula4QPPSW/src/exercicio/ArquivoTesteGIT.java
+++ ExercicioAula4QPPSW/src/exercicio/ArquivoTesteGIT.java
@@ -13,5 +13,21 @@ public class ArquivoTesteGIT {
     
     private String nome;
     private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
             
 }
