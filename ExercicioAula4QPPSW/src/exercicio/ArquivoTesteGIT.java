221a031ae6550ed9b780c64be4c52564ecec3657
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2019-05-29 20:48:53 -0300

    projeto netbeans

---
+++ ExercicioAula4QPPSW/src/exercicio/ArquivoTesteGIT.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio;
+
+/**
+ *
+ * @author marcelo.soares
+ */
+public class ArquivoTesteGIT {
+    
+    private String nome;
+    private int idade;
+            
+}
